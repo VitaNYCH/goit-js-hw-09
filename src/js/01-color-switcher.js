bb9712e2041e46b491d3bdbf826ceb0785c460ca
VitaNYCH <vitanychvoloda@yahoo.com>
2023-03-10 00:02:01 +0200

    Made color - switcher/ task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,55 @@
+import '../css/common.css';
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
+
+refs.startBtn.addEventListener('click', () => { colorSwitcher.start(); });
+
+refs.stopBtn.addEventListener('click', () => { colorSwitcher.stop(); });
+refs.stopBtn.disabled = true;
+
+
+const colorSwitcher = {
+
+    intervalId: null,
+    isActive: false,
+    
+    start() {
+        if (this.isActive) {
+            return;
+        }
+    refs.startBtn.disabled = true;    
+    refs.stopBtn.disabled = false;
+    this.isActive = true;
+        
+   this.intervalId = setInterval(() => {
+       refs.bodyEl.style.backgroundColor = getRandomHexColor();
+    console.log('changed color: ', getRandomHexColor());
+}, 1000); 
+    },
+
+    stop() {
+    
+      refs.startBtn.removeEventListener('click', () => {colorSwitcher.start();});
+        clearInterval(this.intervalId);
+        refs.startBtn.disabled = false;
+        refs.stopBtn.disabled = true;
+        refs.bodyEl.style.backgroundColor = null;  
+        this.isActive = false;
+    },      
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+
+
+
+
 
